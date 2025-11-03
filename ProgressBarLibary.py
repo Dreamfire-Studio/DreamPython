8c08bc216b54541f595281600c8dfef8f1363d2e
Joshua Filer <68482902+Closeplanet2@users.noreply.github.com>
2025-11-03 14:34:35 +0000

    Updates

--- ProgressBarLibary.py
+++
@@ -1,6 +0,0 @@
-
-class ProgressBarLibary:
-    def  progress_bar(self, title, progress, total):
-        percent = 100 * (progress / float(total))
-        bar = '█' * int(percent) + '-' * (100 - int(percent))
-        print(f"\r{title} |{bar}| {percent:.2f}% ({progress}/{total})", end="")
